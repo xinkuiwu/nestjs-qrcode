7fa629c9429291fe988713e30bf536c628e31195
xinkuiwu <1079894635@qq.com>
2024-01-04 22:25:41 +0800

    feat: qrcode login

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { JwtModule } from "@nestjs/jwt";
+
+@Module({
+  imports: [JwtModule.register({
+    secret:'wu'
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
